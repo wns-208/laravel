441ef62aec76dfb77cd2ae4a0fbab519eba2b689
wns-208 <lordwillias@gmail.com>
2026-03-17 14:47:48 -0300

    17-03-26

--- routes/web.php
+++ routes/web.php
@@ -12,15 +12,20 @@ use App\Http\Middleware\LogAcessoMiddleware;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('/', [App\Http\Controllers\Principal::class, 'principal'])->name('pagina-principal');
+
 Route::prefix('publico')->group(function(){
 
-    Route::get('/', [App\Http\Controllers\Principal::class, 'principal']);
+    
     Route::get('/contato/{nome}', [App\Http\Controllers\Principal::class, 'contato']);
     Route::get('/contato/{nome}/{sobrenome}', [App\Http\Controllers\Principal::class, 'contatoNomeCompleto']);
     Route::get('/contato/{nome}/{sobrenome}/{mensagem}', [App\Http\Controllers\Principal::class, 'contatoMensagem']);
     Route::get('/contato/{nome}/{sobrenome}/{mensagem}/{telefone}/{email?}', [App\Http\Controllers\Principal::class, 'contatoTelefone']);
 });
 
-
+route::fallback(function(){
+    echo "A rota acessada não existe!";
+    echo "<a href='" . route('pagina-principal')  . "'>Voltar</a>";
+});
 
 
