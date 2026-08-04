81bb8f57abcd5cdd71d6cdab904de2c22b2e9afd
wns-208 <lordwillias@gmail.com>
2026-08-04 14:49:21 -0300

    usamos tinker

---
+++ app/Models/aluno.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class aluno extends Model
+{
+    protected $table = "aluno";
+    protected $fillable = ['nome', 'email'];
+    use HasFactory;
+}
